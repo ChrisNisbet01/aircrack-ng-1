943910373b08982f4261243ac99a9fc0a6c533cd
Chris Nisbet <nischris@gmail.com>
2019-08-27 12:48:31 +1200

    Reduced code duplication relating to filtering/logging of APs

--- src/airodump-ng/airodump-ng.c
+++ src/airodump-ng/airodump-ng.c
@@ -105,6 +105,7 @@
 #include "terminal.h"
 #include "ivs_log.h"
 #include "probe_request.h"
+#include "ap_filter.h"
 
 #define DEFAULT_CHANNEL_WIDTH_MHZ 20
 #define ONE_HOUR (60 * 60)
@@ -383,58 +384,6 @@ static void clear_ap_marking(struct ap_list_head * const ap_list)
     }
 }
 
-static bool ap_has_required_security(
-    unsigned int ap_security, 
-    unsigned int required_security)
-{
-    bool const has_required =
-        ap_security == 0
-        || required_security == 0
-        || (ap_security & required_security) != 0;
-
-    return has_required;
-}
-
-static bool ap_should_be_printed(
-    struct local_options const * const options,
-    struct AP_info const * const ap_cur,
-    bool const check_for_broadcast)
-{
-    bool should_print;
-
-    REQUIRE(ap_cur != NULL);
-
-    if (ap_cur->nb_pkt < options->min_pkts
-        || (time(NULL) - ap_cur->tlast) > options->berlin)
-    {
-        should_print = false;
-        goto done;
-    }
-
-    if (check_for_broadcast && MAC_ADDRESS_IS_BROADCAST(&ap_cur->bssid))
-    {
-        should_print = false;
-        goto done;
-    }
-
-    if (!ap_has_required_security(ap_cur->security, options->encryption_filter))
-    {
-        should_print = false;
-        goto done;
-    }
-
-    if (is_filtered_essid(&options->essid_filter, ap_cur->essid))
-    {
-        should_print = false;
-        goto done;
-    }
-
-    should_print = true;
-
-done:
-    return should_print;
-}
-
 static void color_off(struct local_options * const options)
 {
     clear_ap_marking(&options->ap_list);
@@ -453,8 +402,14 @@ static void color_on(struct local_options * const options)
     TAILQ_FOREACH_REVERSE(ap_cur, &options->ap_list, ap_list_head, entry)
 	{
 		struct ST_info * st_cur;
+        bool const check_broadcast = true;
 
-        if (!ap_should_be_printed(options, ap_cur, true))
+        if (!ap_should_be_logged(ap_cur, 
+                                 options->berlin, 
+                                 options->encryption_filter, 
+                                 &options->essid_filter, 
+                                 check_broadcast,
+                                 options->min_pkts))
         {
             continue;
         }
@@ -3441,12 +3396,9 @@ write_packet:
             }
         }
 
-        if (!ap_has_required_security(ap_cur->security, options->encryption_filter))
-        {
-            return;
-        }
-
-        if (is_filtered_essid(&options->essid_filter, ap_cur->essid))
+        if (!ap_has_required_security_and_essid(ap_cur, 
+                                                options->encryption_filter, 
+                                                &options->essid_filter))
         {
             return;
         }
@@ -3746,7 +3698,14 @@ static void dump_print(
 
         TAILQ_FOREACH_REVERSE(ap_cur, &options->ap_list, ap_list_head, entry)
 		{
-            if (!ap_should_be_printed(options, ap_cur, true))
+            bool const check_broadcast = true;
+
+            if (!ap_should_be_logged(ap_cur,
+                                     options->berlin,
+                                     options->encryption_filter,
+                                     &options->essid_filter,
+                                     check_broadcast,
+                                     options->min_pkts))
 			{
                 if (options->p_selected_ap == ap_cur)
 				{ //the selected AP is skipped (will not be printed), we have to go to the next printable AP
@@ -3758,8 +3717,15 @@ static void dump_print(
 						ap_tmp = TAILQ_NEXT(ap_cur, entry);
 						if (ap_tmp != NULL)
 						{
+                            bool const check_broadcast = true;
+
                             while ((NULL != (options->p_selected_ap = ap_tmp))
-                                   && !ap_should_be_printed(options, ap_tmp, true))
+                                   && !ap_should_be_logged(ap_tmp,
+                                                           options->berlin,
+                                                           options->encryption_filter,
+                                                           &options->essid_filter,
+                                                           check_broadcast,
+                                                           options->min_pkts))
 							{
 								ap_tmp = TAILQ_NEXT(ap_tmp, entry);
 							}
@@ -3769,8 +3735,15 @@ static void dump_print(
 							ap_tmp = TAILQ_PREV(ap_cur, ap_list_head, entry);
 							if (ap_tmp != NULL)
 							{
+                                bool const check_broadcast = true;
+
                                 while ((NULL != (options->p_selected_ap = ap_tmp))
-                                       && !ap_should_be_printed(options, ap_tmp, true))
+                                       && !ap_should_be_logged(ap_tmp,
+                                                               options->berlin,
+                                                               options->encryption_filter,
+                                                               &options->essid_filter,
+                                                               check_broadcast,
+                                                               options->min_pkts))
 								{
 									ap_tmp = TAILQ_PREV(ap_tmp, ap_list_head, entry);
                                 }
@@ -3783,8 +3756,15 @@ static void dump_print(
 						ap_tmp = TAILQ_PREV(ap_cur, ap_list_head, entry);
 						if (ap_tmp != NULL)
 						{
+                            bool const check_broadcast = true;
+
                             while ((NULL != (options->p_selected_ap = ap_tmp))
-                                   && !ap_should_be_printed(options, ap_tmp, true))
+                                   && !ap_should_be_logged(ap_tmp,
+                                                           options->berlin,
+                                                           options->encryption_filter,
+                                                           &options->essid_filter,
+                                                           check_broadcast,
+                                                           options->min_pkts))
 							{
 								ap_tmp = TAILQ_PREV(ap_tmp, ap_list_head, entry);
 							}
@@ -3794,8 +3774,15 @@ static void dump_print(
 							ap_tmp = TAILQ_NEXT(ap_cur, entry);
 							if (ap_tmp != NULL)
 							{
+                                bool const check_broadcast = true;
+
                                 while ((NULL != (options->p_selected_ap = ap_tmp))
-                                       && !ap_should_be_printed(options, ap_tmp, true))
+                                       && !ap_should_be_logged(ap_tmp,
+                                                               options->berlin,
+                                                               options->encryption_filter,
+                                                               &options->essid_filter,
+                                                               check_broadcast,
+                                                               options->min_pkts))
                                 {
 									ap_tmp = TAILQ_NEXT(ap_tmp, entry);
                                 }
@@ -4184,7 +4171,20 @@ static void dump_print(
 
         TAILQ_FOREACH_REVERSE(ap_cur, &options->ap_list, ap_list_head, entry)
 		{
-            if (!ap_should_be_printed(options, ap_cur, false))
+            bool const check_broadcast = false;
+
+            if (!ap_should_be_logged(ap_cur,
+                                     options->berlin,
+                                     options->encryption_filter,
+                                     &options->essid_filter,
+                                     check_broadcast,
+                                     options->min_pkts))
+            {
+                continue;
+            }
+
+            if (MAC_ADDRESS_IS_BROADCAST(&ap_cur->bssid)
+                && options->asso_client)
             {
                 continue;
             }
